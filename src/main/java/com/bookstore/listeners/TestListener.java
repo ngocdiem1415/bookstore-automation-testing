b32b14e93f22f5c6c87b478965fefcd74234ae3d
NgocDiem <22130038@st.hcmuaf.edu.vn>
2026-04-17 10:57:26 +0700

    feat: add Extent Reports and TestNG listeners for test tracking

--- src/main/java/com/bookstore/listeners/TestListener.java
+++ src/main/java/com/bookstore/listeners/TestListener.java
@@ -1,4 +1,35 @@
 package com.bookstore.listeners;
 
-public class TestListener {
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.bookstore.report.ExtentManager;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class TestListener implements ITestListener {
+    private static ExtentReports extent = ExtentManager.createInstance();
+    private static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        ExtentTest extentTest = extent.createTest(result.getMethod().getDescription());
+        test.set(extentTest);
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        double duration = (result.getEndMillis() - result.getStartMillis()) / 1000.0;
+        test.get().pass("Test Case Passed trong " + duration + "s");
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        test.get().fail(result.getThrowable());
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        extent.flush();
+    }
 }
--- src/main/java/com/bookstore/report/ExtentManager.java
+++ src/main/java/com/bookstore/report/ExtentManager.java
@@ -1,4 +1,29 @@
 package com.bookstore.report;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
 public class ExtentManager {
+    private static ExtentReports extent;
+
+    public static ExtentReports createInstance() {
+        String fileName = "bookstore-automation-report.html";
+        ExtentSparkReporter htmlReporter = new ExtentSparkReporter("target/reports/" + fileName);
+
+        htmlReporter.config().setTheme(Theme.STANDARD);
+        htmlReporter.config().setDocumentTitle("Bookstore Automation Report");
+        htmlReporter.config().setEncoding("utf-8");
+        htmlReporter.config().setReportName("Kết quả kiểm thử tự động - Bookstore Project");
+
+        extent = new ExtentReports();
+        extent.attachReporter(htmlReporter);
+
+        extent.setSystemInfo("Project Name", "Bookstore Ecommerce System");
+        extent.setSystemInfo("Author", "Nguyen Thi Ngoc Diem");
+        extent.setSystemInfo("Environment", "Localhost");
+        extent.setSystemInfo("Browser", "Chrome");
+
+        return extent;
+    }
 }
